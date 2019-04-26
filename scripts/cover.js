b128c68355db4361f505d6af25f93b3c586c9977
wangjian <wangjian@didpinche.com>
2019-04-26 09:52:39 +0800

    node-script

---
+++ scripts/cover.js
@@ -0,0 +1,17 @@
+const {rm, cp, mkdir, exec, echo} = require('shelljs');
+const chalk = require('chalk');
+
+console.log(chalk.green('1. remove old voverage reports...'));
+rm('-rf', 'coverage');
+rm('-rf', '.nyc_output');
+
+console.log(chalk.green('2. run test and collect new coverage...'));
+exec('nyc --reporter=html npm run test');
+
+console.log(chalk.green('3. archive coverage report by version...'));
+mkdir('-p', 'coverage_archive/$npm_package_version');
+cp('-r', 'coverage/*', 'coverage_archive/$npm_package_version');
+
+console.log(chalk.green('4. open coverage report fot preview...'));
+exec('npm-run-all --parallel cover:serve cover:open');
+
