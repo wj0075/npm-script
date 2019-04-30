b583522ba84d8b1d3308ed3e1a423a21889b48b8
wangjian <wangjian@didpinche.com>
2019-04-30 13:46:29 +0800

    devops init

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static('./dist'));
+app.use(morgan('combined'));
+
+app.listen(port, err => {
+  if (err) {
+    console.log('server start error', err);
+    process.exit(1);
+  }
+
+  console.log(`server started at port ${port}`);
+})
